17fd1fa727aa7879c223e73fef41fe81a03a0cf0
Gitu Mbugua <gmbugua38@gmail.com>
2023-08-22 09:53:00 +0300

    Initial commit

---
+++ base/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+TASK_STATUS = (
+    ("INCOMPLETE", "Incomplete"),
+    ("COMPLETE", "Complete"),
+    ("IN PROGRESS", "In progress"),
+)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    due = models.DateTimeField()
+    status = models.CharField(max_length=10,
+                  choices=TASK_STATUS,
+                  default="JANUARY")
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name    
+    
+    @classmethod
+    def getAllTasks(cls):
+        tasks = cls.objects.all().order_by('-created')
+        return tasks
