5e032571f0918ac14b384bcc5a49864f259d7bae
Gitu Mbugua <gmbugua38@gmail.com>
2023-08-22 12:48:56 +0300

    Added functionality to update and delete tasks

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('create-task', views.createTask, name="create-task")
+    path('task/<str:pk>/', views.task, name="task"),
+    path('create-task', views.createTask, name="create-task"),
+    path('update-task/<str:pk>/', views.updateTask, name="update-task"),
+    path('delete-task/<str:pk>/', views.deleteTask, name="delete-task"),
 ]
--- base/views.py
+++ base/views.py
@@ -8,6 +8,12 @@ def home(request):
     context = {'tasks':tasks}
     return render(request, 'base/home.html', context)
 
+def task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    context = {'task':task}
+    return render(request, 'base/task.html', context)
+
 def createTask(request):
     form = TaskForm()
     if request.method == 'POST':
@@ -20,3 +26,27 @@ def createTask(request):
 
     context = {'form':form}
     return render(request, 'base/task_form.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            form = TaskForm()
+    context = {'form':form}
+    return render(request, 'base/task_form.html', context)
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html', {'obj':task})
+    
+
